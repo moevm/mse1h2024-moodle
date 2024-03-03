343d3189b9dec67f25c3fee2f3d38adf624d3c3d
Test User <polina.koroleva05@mail.ru>
2024-03-03 16:52:47 +0300

    правки
    переименовала папку client, поправила форматирование routes, убрала README и gitignor

--- client/src/router.js
+++ client/src/router.js
@@ -3,14 +3,8 @@ import Main from "./components/Main"
 import NotFound from "./components/NotFound"
 const routes = [
 { path: '/main', name: 'Main', component: Main },
-{ path: '/', name: 'Auth',
-// Создаст компонент, lazy-подгружаемый при первом обращении
-component: () => import('./components/Auth.vue')
-},
+{ path: '/', name: 'Auth', component: () => import('./components/Auth.vue')},
 { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound }
 ]
-const router = createRouter({
-history: createWebHashHistory(),
-routes
-})
+const router = createRouter({history: createWebHashHistory(), routes})
 export default router
