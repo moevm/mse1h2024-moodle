06355e163cc2c287ad88b17e90d3ba24ff2399b2
DaniiLBez <dvbezzubov@stud.eltech.ru>
2024-03-05 16:54:18 +0300

    Добавлен скрипт инициализации БД

---
+++ server/init-mongo.js
@@ -0,0 +1,77 @@
+db = db.getSiblingDB('moodle-statistics');
+
+db.users.insertMany([
+    {
+        "name": "Иван",
+        "surname": "Иванов",
+        "lastname": "Иванович",
+        "email": "ivan@mail.ru",
+        "position": "admin",
+        "password": "bhewrtfm3klmt3"
+    },
+    {
+        "name": "Петров",
+        "surname": "Петр",
+        "lastname": "Петрович",
+        "email": "petr@mail.ru",
+        "position": "regular",
+        "password": "bhewrsdfsdfsdwlmt3"
+    },
+    {
+        "name": "Сидоров",
+        "surname": "Федор",
+        "lastname": "Федорович",
+        "email": "fedor@mail.ru",
+        "position": "regular",
+        "password": "bhsdgdfgergef3klmt3"
+    }
+])
+
+db.statistics.insertMany([
+    {
+        "student": "Диана Романенко",
+        "groupnumber": 9303,
+        "email": "diana@mail.ru",
+        "course": "Курс молодого бойца",
+        "session": "",
+        "actions": [
+            {
+            "date": "2024-02-02",
+            "time": "14:00:00",
+            "page": "http://e.moevm.info/some_course",
+            "element_type": "button",
+            "element_name": "сохранить",
+            "action_type": "conversation",
+            "event": "mousedown"
+            }
+        ]
+    },
+    {
+        "student": "Беззубов Даниил",
+        "groupnumber": 1303,
+        "email": "daniil@mail.ru",
+        "course": "Курс молодого бойца",
+        "session": "",
+        "actions": [
+            {
+                "date": "2024-02-02",
+                "time": "14:00:00",
+                "page": "http://e.moevm.info/some_course",
+                "element_type": "button",
+                "element_name": "сохранить",
+                "action_type": "conversation",
+                "event": "mousedown"
+            },
+            {
+                "date": "2024-02-02",
+                "time": "14:01:00",
+                "page": "http://e.moevm.info/some_course",
+                "element_type": "button",
+                "element_name": "следующая страница",
+                "action_type": "conversation",
+                "event": "mousedown"
+            }
+        ]
+    }
+])
+
