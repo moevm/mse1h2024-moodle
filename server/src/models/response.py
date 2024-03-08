c51127f7511d15a57a6b802c9cb4226ea6208fc1
DaniiLBez <dvbezzubov@stud.eltech.ru>
2024-03-08 22:52:20 +0300

    update models

---
+++ server/src/models/response.py
@@ -0,0 +1,10 @@
+def response_model(data, message):
+    return {
+        "data": [data],
+        "code": 200,
+        "message": message,
+    }
+
+
+def error_response_model(error, code, message):
+    return {"error": error, "code": code, "message": message}
--- server/src/models/session_data.py
+++ server/src/models/session_data.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr, HttpUrl
+from pydantic import BaseModel, EmailStr, HttpUrl, Field
 from datetime import date, time
 from typing import List
 from enum import Enum
@@ -17,19 +17,44 @@ class EventType(str, Enum):
 
 
 class Payload(BaseModel):
-    date: date
-    time: time
-    page: HttpUrl
-    element_type: ElementType
-    element_name: str
-    action_type: ActionType
-    event: ElementType
+    date: date = Field(default_factory=date.today)
+    time: time = Field(default_factory=time)
+    page: HttpUrl = Field(...)
+    element_type: ElementType = Field(...)
+    element_name: str = Field(...)
+    action_type: ActionType = Field(...)
+    event: ElementType = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "date": date.today(),
+                "time": time,
+                "page": "https://example.com",
+                "element_type": ElementType.button,
+                "element_name": "отправить",
+                "action_type": ActionType.conversation,
+                "event": EventType.mousedown
+            }
+        }
 
 
 class SessionData(BaseModel):
-    student: str
-    group: int
-    email: EmailStr
-    course: str
-    session: str
-    actions: List[Payload]
+    student: str = Field(...)
+    group: int = Field(...)
+    email: EmailStr = Field(...)
+    course: str = Field(...)
+    session: str = Field(...)
+    actions: List[Payload] = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "student": "Иванов Иван",
+                "group": 1303,
+                "email": "iiivanov@edu.ru",
+                "course": "Курс молодого бойца",
+                "session": "sessionId",
+                "actions": []
+            }
+        }
