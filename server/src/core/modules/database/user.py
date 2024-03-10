b5cf38400fc6b42fb210195586ecbbd281fb46e4
DaniiLBez <dvbezzubov@stud.eltech.ru>
2024-03-11 02:03:35 +0300

    added errors raising

--- server/src/core/modules/database/user.py
+++ server/src/core/modules/database/user.py
@@ -1,4 +1,7 @@
 from bson.objectid import ObjectId
+from fastapi import HTTPException, status
+
+from src.core.modules.database.errors import RepoNotFoundError, RepoAlreadyExistsError, RepoEmptyDataError
 
 
 class MongoUserRepo:
@@ -8,44 +11,35 @@ class MongoUserRepo:
     async def get_all_users(self) -> list:
         users = []
         for user in await self.client.users.find():
-            users.append(user_helper(user))
+            users.append(user)
         return users
 
     async def get_user(self, user_id: str) -> dict:
         user = await self.client.users.find_one({"_id": ObjectId(user_id)})
         if user:
-            return user_helper(user)
+            return user
+        raise RepoNotFoundError(f"user with id {user_id} not found")
 
     async def add_user(self, user_data: dict) -> dict:
+        if await self.client.users.find_one({"email": user_data["email"]}) is not None:
+            raise RepoAlreadyExistsError(f"user with email {user_data['email']} already exists")
         user = await self.client.users.insert_one(user_data)
         new_user = await self.client.users.find_one({"_id": user.inserted_id})
-        return user_helper(new_user)
+        return new_user
 
-    async def update_user(self, user_id: str, data: dict) -> bool:
+    async def update_user(self, user_id: str, data: dict) -> dict:
         if len(data) < 1:
-            return False
+            raise RepoEmptyDataError("empty request body")
         user = await self.client.users.find_one({"_id": ObjectId(user_id)})
         if not user:
-            return False
+            raise RepoNotFoundError(f"user with id {user_id} not found")
         updated_user = await self.client.users.update_one(
             {"_id": ObjectId(user_id)}, {"$set": data}
         )
-        return updated_user is not None
+        return updated_user
 
-    async def delete_user(self, user_id: str) -> bool:
+    async def delete_user(self, user_id: str):
         user = await self.client.users.find_one({"_id": ObjectId(user_id)})
         if user:
             await self.client.users.delete_one({"_id": ObjectId(user_id)})
-            return True
-
-
-def user_helper(user) -> dict:
-    return {
-        "id": str(user["_id"]),
-        "name": user["name"],
-        "surname": user["surname"],
-        "lastname": user["lastname"],
-        "email": user["email"],
-        "password": user["password"],
-        "position": user["position"],
-    }
+        raise RepoNotFoundError(f"user with id {user_id} not found")
