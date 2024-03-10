6d8f368fde401aa15f7240671f524bbce8c781ac
DaniiLBez <dvbezzubov@stud.eltech.ru>
2024-03-11 02:03:35 +0300

    added errors raising

--- server/src/core/modules/database/statistics.py
+++ server/src/core/modules/database/statistics.py
@@ -1,5 +1,7 @@
 from bson.objectid import ObjectId
 
+from src.core.modules.database.errors import RepoNotFoundError
+
 
 class MongoStatisticRepo:
     def __init__(self, client):
@@ -8,33 +10,22 @@ class MongoStatisticRepo:
     async def get_all_sessions(self) -> list:
         stats = []
         for session in self.client.statistics.find():
-            stats.append(session_data_helper(session))
+            stats.append(session)
         return stats
 
     async def get_session(self, record_id: str) -> dict:
         stat = await self.client.statistics.find_one({"_id": ObjectId(record_id)})
         if stat:
-            return session_data_helper(stat)
+            return stat
+        raise RepoNotFoundError(f'record {record_id} not found')
 
     async def add_session(self, session_data: dict) -> dict:
         stat = await self.client.statistics.insert_one(session_data)
         new_stat = await self.client.statistics.find_one({"_id": stat.inserted_id})
-        return session_data_helper(new_stat)
+        return new_stat
 
-    async def delete_session(self, record_id: str) -> bool:
+    async def delete_session(self, record_id: str):
         stat = await self.client.statistics.find_one({"_id": ObjectId(record_id)})
         if stat:
             await self.client.statistics.delete_one({"_id": ObjectId(record_id)})
-            return True
-
-
-def session_data_helper(session_data) -> dict:
-    return {
-        "id": str(session_data["_id"]),
-        "student": session_data["student"],
-        "group": session_data["group"],
-        "email": session_data["email"],
-        "course": session_data["course"],
-        "session": session_data["session"],
-        "actions": session_data["actions"]
-    }
+        raise RepoNotFoundError(f'record {record_id} not found')
