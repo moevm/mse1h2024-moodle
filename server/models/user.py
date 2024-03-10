d1b5e90c58395da4bb39e24f90f70c8aa803ccb6
DaniiLBez <dvbezzubov@stud.eltech.ru>
2024-03-11 02:01:16 +0300

    ObjectId problem fixes: deleted unused funcs, added field "id" to models

--- server/models/user.py
+++ server/models/user.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+from bson import ObjectId
 from pydantic import BaseModel, EmailStr, Field
 from enum import Enum
 
@@ -8,12 +11,34 @@ class Role(str, Enum):
 
 
 class User(BaseModel):
+    id: ObjectId = Field(default_factory=ObjectId, alias="_id")
     name: str = Field(...)
     surname: str = Field(...)
     lastname: str = Field(...)
     email: EmailStr = Field(...)
     position: Role = Field(...)
     password: str = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Иван",
+                "surname": "Иванов",
+                "lastname": "Иванович",
+                "email": "iiivanov@edu.ru",
+                "position": Role.regular,
+                "password": "sdfsdfwgesdgcx"
+            }
+        }
+
+
+class UpdateUser(BaseModel):
+    name: Optional[str]
+    surname: Optional[str]
+    lastname: Optional[str]
+    email: Optional[EmailStr]
+    position: Optional[Role]
+    password: Optional[str]
 
     class Config:
         schema_extra = {
