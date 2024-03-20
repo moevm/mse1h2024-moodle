b0df15beb16a1287f1e1778d987ffdcc9ad66873
LeraChernyakova <2215391@mail.ru>
2024-03-21 01:34:12 +0300

    Реализована страница создания пользователя.

--- client/src/router.js
+++ client/src/router.js
@@ -5,6 +5,7 @@ const routes = [
     { path: '/start', redirect: '/e.moevm.statistics/auth'},
     { path: '/e.moevm.statistics/auth', name: 'Authorization', component: () => import('./components/Pages/AuthorizationPage.vue')},
     { path: '/e.moevm.statistics/statistics', name: 'Statistics', component: () => import('./components/Pages/StatisticsPage.vue')},
+    { path: '/e.moevm.statistics/user', name: 'CreateUser', component: () => import('./components/Pages/CreateUserPage.vue')},
 ]
 
 const router = createRouter({
