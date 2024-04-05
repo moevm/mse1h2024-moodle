19b9dbc2adb1116fc833761861fb98b110a178d9
LeraChernyakova <2215391@mail.ru>
2024-04-06 00:36:24 +0300

    #32 Implement a search for data in the table

---
+++ client/src/components/Filters/Filters.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container class="filters">
+    <slot></slot>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Filters'
+};
+</script>
+
+<style>
+@import '@/colors.css';
+
+.filters {
+  max-width: 95%;
+  padding-left: 0;
+  padding-right: 0;
+  justify-self: center;
+}
+</style>
