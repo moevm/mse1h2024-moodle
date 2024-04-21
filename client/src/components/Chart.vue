3dc6ef50c2b1a8f8e6dc991ccf9d73bd452fe59f
Test User <polina.koroleva05@mail.ru>
2024-04-21 22:25:01 +0300

    done date format in tooltips

--- client/src/components/Chart.vue
+++ client/src/components/Chart.vue
@@ -15,9 +15,10 @@ import {
   Title,
   Tooltip,
   Legend,
-BarElement,
+  BarElement,
 } from "chart.js";
 import { Bar } from "vue-chartjs";
+import "chartjs-adapter-moment";
 
 ChartJS.register(
   CategoryScale,
@@ -38,52 +39,56 @@ export default {
       type: Array,
       required: true,
     },
-    search:{
+    search: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
   data() {
     return {
       chartOptions: {
         scales: {
           x: {
-            type: 'linear',
+            type: "time",
+            time: {
+              displayFormats: {
+                day: "DD MMM YYYY HH:mm:ss.sss",
+              },
+            },
             barThickness: 10,
             title: {
               display: true,
-              text: "Временной промежуток"
+              text: "Временной промежуток",
             },
             display: true,
-            ticks:{
-              // autoSkip: false,
+            ticks: {
               maxTicksLimit: 20,
               minRotation: 80,
-              callback: function(value){
-                let date = new Date(value);
-                const hours = date.getHours().toString().padStart(2, '0');
-                const minutes = date.getMinutes().toString().padStart(2, '0');
-                const seconds = date.getSeconds().toString().padStart(2, '0');
-                const milliseconds = date.getMilliseconds().toString().padStart(3, '0');
-                return `${date.toLocaleDateString('ru-RU')} ${hours}:${minutes}:${seconds}.${milliseconds}`;
-                }
-            }
+              
+            },
           },
           y: {
             title: {
               display: true,
-              text: "Количество событий"
+              text: "Количество событий",
             },
             suggestedMin: 0,
             display: true,
             ticks: {
-              stepSize: 1
+              stepSize: 1,
             },
           },
         },
-        zoom: {
-          enabled: true,
-          drag: true
+        plugins: {
+          tooltip: {
+            callbacks: {
+              title: function (tooltipItem) {
+                console.log("tooltipItem", tooltipItem);
+                let date = tooltipItem[0].parsed.x;
+                return new Date(date).toISOString();
+              },
+            },
+          },
         },
         responsive: true,
       },
@@ -100,50 +105,62 @@ export default {
         if (a.Date > b.Date) return 1;
         return 0;
       }
-      console.log(typeof this.search, this.search)
-      let searchLow = '';
-      if(this.search){
-        searchLow = this.search.toLocaleLowerCase()
-      };
-      let cloneInfo = this.info.filter((action)=>{
-        return action.studentId.toString().toLocaleLowerCase().includes(searchLow)
-         || action.FIO.toLocaleLowerCase().includes(searchLow)
-         || action.course.toLocaleLowerCase().includes(searchLow) 
-         || action.typeAction.toLocaleLowerCase().includes(searchLow) 
-         || action.action.toLocaleLowerCase().includes(searchLow)
-         || action.page.toLocaleLowerCase().includes(searchLow) 
-         || action.time.toLocaleLowerCase().includes(searchLow) 
-         || action.date.toLocaleLowerCase().includes(searchLow)
-      })
+      console.log(typeof this.search, this.search);
+      let searchLow = "";
+      if (this.search) {
+        searchLow = this.search.toLocaleLowerCase();
+      }
+      let cloneInfo = this.info.filter((action) => {
+        return (
+          action.studentId.toString().toLocaleLowerCase().includes(searchLow) ||
+          action.FIO.toLocaleLowerCase().includes(searchLow) ||
+          action.course.toLocaleLowerCase().includes(searchLow) ||
+          action.typeAction.toLocaleLowerCase().includes(searchLow) ||
+          action.action.toLocaleLowerCase().includes(searchLow) ||
+          action.page.toLocaleLowerCase().includes(searchLow) ||
+          action.time.toLocaleLowerCase().includes(searchLow) ||
+          action.date.toLocaleLowerCase().includes(searchLow)
+        );
+      });
       cloneInfo.sort(compareDate);
-      let arr = cloneInfo
+      let arr = cloneInfo;
       let limit = 20;
       let delta = 100;
-      if(cloneInfo.length > limit){
+      if (cloneInfo.length > limit) {
         let min = cloneInfo.at(0).Date;
         let max = cloneInfo.at(-1).Date;
-        delta = (max - min)/(limit*100)
+        delta = (max - min) / (limit * 100);
       }
 
       const dateCount = [];
       arr.forEach((action) => {
         let key = +action.Date;
-        let ind = dateCount.findIndex(element => Math.abs(element[0]-key)<delta)
-        if (ind<0) {
+        let ind = dateCount.findIndex(
+          (element) => Math.abs(element[0] - key) < delta
+        );
+        if (ind < 0) {
           dateCount.push([key, 1]);
         } else {
-          dateCount[ind][1] +=1;
+          dateCount[ind][1] += 1;
         }
       });
       dateCount.forEach(([key, value]) => {
-        labels.push(key)
+        labels.push(key);
         data.push(new Date(value));
       });
       console.log(data);
       return {
         labels,
         datasets: [
-          {data,showLine: true, backgroundColor: "#61dafb", borderColor: "#61dafb", label: "график", label: "активность", barThickness: 2},
+          {
+            data,
+            showLine: true,
+            backgroundColor: "#61dafb",
+            borderColor: "#61dafb",
+            label: "график",
+            label: "активность",
+            barThickness: 4,
+          },
         ],
       };
     },
