728edc9a439a81b021a3a8c22756763034fa2746
LeraChernyakova <2215391@mail.ru>
2024-04-24 02:02:41 +0300

    add selenium test on create user page

--- client/src/test/test-selenium.js
+++ client/src/test/test-selenium.js
@@ -5,6 +5,8 @@ const driver = new Builder()
     .forBrowser('MicrosoftEdge')
     .build();
 
+let allUsersCount = 0;
+
 async function test(url) {
     const logFilePath = 'test.txt';
     if (fs.existsSync(logFilePath)) {
@@ -36,6 +38,7 @@ async function test(url) {
             logStream.write('Open page: ' + currentUrl + '\n');
             await testStatisticPage(logStream);
             await testAllUsersPage(logStream);
+            await testAddUserPage(logStream);
         }
         else {
             logStream.write('ERROR! Catch error then sign in\n');
@@ -96,16 +99,55 @@ async function testAllUsersPage(logStream) {
         logStream.write('Open page: ' + currentUrl + '\n');
         await driver.wait(until.elementLocated(By.className('v-data-table__tr')), 100);
         let users = await driver.findElements(By.className("v-data-table__tr"));
-        let count = users.length;
-        if (count === 3) {
-            logStream.write('SUCCESS! Users were displayed correct.\n');
+        allUsersCount = users.length;
+        if (allUsersCount === 3) {
+            logStream.write('SUCCESS! Users were displayed correct\n');
         }
         else {
-            logStream.write('ERROR! Users were displayed incorrect.\n');
+            logStream.write('ERROR! Users were displayed incorrect\n');
         }
     }
     else {
         logStream.write('ERROR! Redirect to all users page was incorrect\n');
+    }
+}
+
+async function testAddUserPage(logStream) {
+    await driver.get('http://localhost:8081/e.moevm.statistics/user');
+    await driver.sleep(100)
+    const currentUrl = await driver.getCurrentUrl();
+    if (currentUrl === 'http://localhost:8081/e.moevm.statistics/user') {
+        logStream.write('SUCCESS! Redirect to create user page was correct\n');
+        logStream.write('Open page: ' + currentUrl + '\n');
+
+        const email = 'test@mail.ru';
+        const surname = 'Smirnov';
+        const name = 'Ilya';
+        const password = '1234';
+
+        logStream.write('Input email: ' + email + '\n');
+        await driver.findElement(By.id("email-input")).sendKeys(email);
+        logStream.write('Input surname: ' + surname + '\n');
+        await driver.findElement(By.id("surname-input")).sendKeys(surname);
+        logStream.write('Input name: ' + name + '\n');
+        await driver.findElement(By.id("name-input")).sendKeys(name);
+        logStream.write('Input password: ' + password + '\n');
+        await driver.findElement(By.id("password-input")).sendKeys(password);
+        logStream.write('Click on create button\n');
+        await driver.findElement(By.id('create-button')).click();
+        await driver.sleep(100);
+        let alert = await driver.switchTo().alert();
+        let alertText = await alert.getText();
+        logStream.write('Alert message: ' + alertText + '\n');
+        if (alertText === 'Пользователь успешно создан') {
+            logStream.write('SUCCESS! User was created\n');
+        }
+        else {
+            logStream.write('Error! User was not created\n');
+        }
+    }
+    else {
+        logStream.write('ERROR! Redirect to create user page was incorrect\n');
     }
 }
 
