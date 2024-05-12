84fb416722c77a7b6cced48145476f3bdb0113b8
DaniiLBez <dvbezzubov@stud.eltech.ru>
2024-05-12 19:51:04 +0300

    added router for downloading

--- server/src/api/api.py
+++ server/src/api/api.py
@@ -6,3 +6,4 @@ api_router.include_router(statistics.router, prefix="/statistics", tags=["statis
 api_router.include_router(user.router, prefix="/user", tags=["user"])
 api_router.include_router(authorization.router, prefix="/auth", tags=["authorization"])
 api_router.include_router(healthcheck.router, prefix="/healthcheck", tags=["healthcheck"])
+api_router.include_router(statistics.download_router, prefix="/download", tags=["download"])
