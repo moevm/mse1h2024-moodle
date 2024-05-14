f794c3f94f9be47a461f2fc6365e628a8f4ace4f
DaniiLBez <dvbezzubov@stud.eltech.ru>
2024-05-15 01:16:48 +0300

    added endpoints for creation and downloading pages data

--- server/src/api/endpoints/statistics.py
+++ server/src/api/endpoints/statistics.py
@@ -1,5 +1,4 @@
 import os
-import tempfile
 from typing import List
 
 import bson.json_util as json
@@ -13,6 +12,7 @@ from src.core.modules.service.errors import SessionNotFoundError
 from src.depends import get_statistics_service
 from src.models.filter import SessionFilter
 from src.models.session_data import SessionData, CreateSessionData
+from src.models.session_data import PageData, CreatePageData
 
 router = APIRouter()
 download_router = APIRouter()
@@ -86,7 +86,7 @@ async def delete_session(session_id: str):
 
 
 @download_router.get(
-    "/",
+    "/statistics",
     response_description="Download statistics"
 )
 async def download_data(params: SessionFilter = Depends()):
@@ -98,6 +98,40 @@ async def download_data(params: SessionFilter = Depends()):
         return FileResponse(
             os.path.join(os.path.curdir, 'data.json'),
             filename="data.json",
+            media_type='application/octet-stream'
+        )
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
+
+@router.post(
+    "/page",
+    status_code=status.HTTP_201_CREATED,
+    response_model=str,
+    response_model_by_alias=False
+)
+async def create_page(page: CreatePageData = Body()):
+    try:
+        page = jsonable_encoder(page)
+        created_page = await service.create_page(page)
+        return created_page['_id']
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+
+@download_router.get(
+    "/sessions",
+    response_description="Download sessions"
+)
+async def download_sessions():
+    try:
+        data = await service.get_pages()
+        with open(os.path.join(os.path.curdir, 'sessions.json'), mode='w+b') as f:
+            f.truncate(0)
+            f.write(json.dumps(data, ensure_ascii=False).encode('utf-8'))
+        return FileResponse(
+            os.path.join(os.path.curdir, 'sessions.json'),
+            filename="sessions.json",
             media_type='application/octet-stream'
         )
     except Exception as e:
