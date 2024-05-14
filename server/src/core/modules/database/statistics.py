96f012fd384d2f7aa543d9e061348a15c68c9543
DaniiLBez <dvbezzubov@stud.eltech.ru>
2024-05-15 01:14:54 +0300

    added MongoPageRepo implementation

--- server/src/core/modules/database/statistics.py
+++ server/src/core/modules/database/statistics.py
@@ -14,6 +14,7 @@ class MongoStatisticRepo:
         query = filters.query()
         for session in await self.client.statistics.find(query).to_list(length=None):
             session["_id"] = str(session["_id"])
+            session["session_id"] = str(session["session_id"])
             session['actions'] = filter_actions(filters, session['actions'])
             stats.append(session)
         return stats
@@ -22,6 +23,7 @@ class MongoStatisticRepo:
         stat = await self.client.statistics.find_one({"_id": ObjectId(record_id)})
         if stat:
             stat["_id"] = str(stat["_id"])
+            stat["session_id"] = str(stat["session_id"])
             return stat
         raise RepoNotFoundError(f'record {record_id} not found')
 
@@ -29,6 +31,7 @@ class MongoStatisticRepo:
         stat = await self.client.statistics.insert_one(session_data)
         new_stat = await self.client.statistics.find_one({"_id": stat.inserted_id})
         new_stat["_id"] = str(new_stat["_id"])
+        new_stat['session_id'] = str(new_stat['session_id'])
         return new_stat
 
     async def delete_session(self, record_id: str):
@@ -55,3 +58,21 @@ def filter_actions(query: SessionFilter, payload: list) -> list:
     if query.end_timestamp:
         data = filter(lambda item: query.end_timestamp >= item['timestamp'], data)
     return list(data)
+
+
+class MongoPageRepo:
+    def __init__(self, client):
+        self.client = client
+
+    async def create_record(self, page_data: dict) -> dict:
+        page = await self.client.sessions.insert_one(page_data)
+        new_page = await self.client.sessions.find_one({"_id": page.inserted_id})
+        new_page["_id"] = str(new_page["_id"])
+        return new_page
+
+    async def get_all_records(self) -> list:
+        pages = []
+        for page in await self.client.sessions.find().to_list(length=None):
+            page["_id"] = str(page["_id"])
+            pages.append(page)
+        return pages
