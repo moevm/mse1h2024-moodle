84c63085e8b4efcd63750607bc2a6c8b50fd8abc
LeraChernyakova <2215391@mail.ru>
2024-05-21 10:07:12 +0300

    change time in driver.sleep()

--- client/src/test/test.mjs
+++ client/src/test/test.mjs
@@ -7,16 +7,16 @@ describe('test e.moevm.statistics', async function () {
 
     const signIn = async (email,password) => {
         await driver.get('http://client:8080/');
-        await driver.sleep(900);
+        await driver.sleep(700);
         await driver.findElement(By.id("email-input")).sendKeys(email);
         await driver.findElement(By.id("password-input")).sendKeys(password);
         await driver.findElement(By.id('sign-in-button')).click();
-        await driver.sleep(900);
+        await driver.sleep(500);
         return await driver.getCurrentUrl();
     };
 
     const getStatisticsData = async () => {
-        await driver.wait(until.elementLocated(By.className("v-data-table__tr")));
+        await driver.wait(until.elementLocated(By.className("v-data-table__tr")), 300);
         let elements = await driver.findElements(By.className("v-data-table__tr"));
         return elements.length;
     }
@@ -24,19 +24,19 @@ describe('test e.moevm.statistics', async function () {
     const getSearchData = async (emailFilter) => {
         await driver.findElement(By.id("search-email")).sendKeys(emailFilter);
         await driver.findElement(By.id("start-search")).click();
-        await driver.wait(until.elementLocated(By.className('v-data-table__tr')), 100);
+        await driver.wait(until.elementLocated(By.className('v-data-table__tr')), 300);
         let elements = await driver.findElements(By.className("v-data-table__tr"));
         await driver.findElement(By.id("reset-search")).click();
         return elements.length;
     }
 
     const getDataByDate = async (startDate, endDate) => {
-        await driver.sleep(900);
+        await driver.sleep(300);
         const startDateElement = await driver.findElement(By.id("start-date"));
         const endDateElement = await driver.findElement(By.id("end-date"));
         await startDateElement.sendKeys(startDate);
         await endDateElement.sendKeys(endDate);
-        await driver.wait(until.elementLocated(By.className('v-data-table__tr')), 900);
+        await driver.wait(until.elementLocated(By.className('v-data-table__tr')), 300);
         let elements = await driver.findElements(By.className("v-data-table__tr"));
         startDateElement.clear();
         endDateElement.clear();
@@ -49,23 +49,21 @@ describe('test e.moevm.statistics', async function () {
     }
 
     const createUser = async (email, surname, name, password) => {
-        await driver.sleep(900);
         await driver.get('http://client:8080/e.moevm.statistics/all-users');
-        await driver.wait(until.elementLocated(By.className("v-data-table__tr")));
+        await driver.wait(until.elementLocated(By.className("v-data-table__tr")), 300);
         let usersBeforeAdd = await driver.findElements(By.className("v-data-table__tr"));
 
-        await driver.sleep(900);
         await driver.get('http://client:8080/e.moevm.statistics/user');
-        await driver.sleep(900);
+        await driver.sleep(500);
         await driver.findElement(By.id("email-input")).sendKeys(email);
         await driver.findElement(By.id("surname-input")).sendKeys(surname);
         await driver.findElement(By.id("name-input")).sendKeys(name);
         await driver.findElement(By.id("password-input")).sendKeys(password);
         await driver.findElement(By.id('create-button')).click();
-        await driver.sleep(900);
+        await driver.sleep(500);
 
         await driver.get('http://client:8080/e.moevm.statistics/all-users');
-        await driver.wait(until.elementLocated(By.className("v-data-table__tr")));
+        await driver.wait(until.elementLocated(By.className("v-data-table__tr")), 300);
         let usersAfterAdd = await driver.findElements(By.className("v-data-table__tr"));
 
         return await usersAfterAdd.length - usersBeforeAdd.length;
@@ -76,9 +74,9 @@ describe('test e.moevm.statistics', async function () {
         const transformedEmail = email.replace(/[^a-zA-Z0-9]/g, '_');
         const deleteButtonId = `delete-item-${transformedEmail}`;
         await driver.findElement(By.id(deleteButtonId)).click();
-        await driver.sleep(900);
+        await driver.sleep(300);
         await driver.switchTo().alert().accept();
-        await driver.sleep(900);
+        await driver.sleep(300);
         let usersAfterDel = await driver.findElements(By.className("v-data-table__tr"));
         return await usersBeforeDel.length - usersAfterDel.length;
     }
