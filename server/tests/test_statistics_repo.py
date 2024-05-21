e663d1b4ce93a3e93c26c13c9655e444d6593da8
AnjeZenda <baykov200396@gmail.com>
2024-05-21 16:33:36 +0300

    Added tags for more convenient reading

--- server/tests/test_statistics_repo.py
+++ server/tests/test_statistics_repo.py
@@ -7,37 +7,48 @@ from src.core.modules.database.errors import RepoNotFoundError
 
 @pytest.mark.asyncio
 async def test_get_all_sessions():
+    # Arrange
     mock_client = Mock()
     mock_client.statistics.find.return_value.to_list.return_value = find_to_list()
     mock_filter = create_mock_filter()
 
     repository = MongoStatisticRepo(mock_client)
+    
+    # Act
     result = await repository.get_all_sessions(mock_filter)
 
+    # Assert
     assert len(result) == 1
     assert result[0]["session_id"] == "2"
 
 
 @pytest.mark.asyncio
 async def test_get_session_success():
+    # Arrange
     mock_client = Mock()
     record_id = '60d5ec49f1a4a21f6c8b4567'
     mock_client.statistics.find_one.return_value = find_one()
 
     repository = MongoStatisticRepo(mock_client)
+    
+    # Act
     result = await repository.get_session(record_id)
 
+    # Assert
     assert result['_id'] == record_id
 
 
 @pytest.mark.asyncio
 async def test_add_session():
+    # Arrange
     mock_client = Mock()
     record_id = '60d5ec49f1a4a21f6c8b4567'
     mock_client.statistics.insert_one.return_value = insert_one()
     mock_client.statistics.find_one.return_value = find_one()
 
     repository = MongoStatisticRepo(mock_client)
+    
+    # Act
     result = await repository.add_session({
         "actions": [
             {
@@ -56,45 +67,56 @@ async def test_add_session():
         "student_id": 1
     })
 
+    # Assert
     assert result['_id'] == record_id
 
 
 @pytest.mark.asyncio
 async def test_delete_session_success():
+    # Arrange
     mock_client = Mock()
     record_id = '60d5ec49f1a4a21f6c8b4567'
     mock_client.statistics.find_one.return_value = find_one()
     mock_client.statistics.delete_one.return_value = delete_one()
 
     repository = MongoStatisticRepo(mock_client)
+    
+    # Act
     result = await repository.delete_session(record_id)
 
+    # Assert
     assert result.deleted_count == 1
 
 
 @pytest.mark.asyncio
 async def test_get_session_fail():
+    # Arrange
     mock_client = Mock()
     record_id = '60d5ec49f1a4a21f6c8b4567'
     mock_client.statistics.find_one.side_effect = RepoNotFoundError()
 
     repository = MongoStatisticRepo(mock_client)
     try:
+        # Act
         result = await repository.get_session(record_id)
     except Exception as e:
+        # Assert
         assert isinstance(e, RepoNotFoundError)
 
 
 @pytest.mark.asyncio
 async def test_delete_session_fail():
+    # Arrange
     mock_client = Mock()
     record_id = '60d5ec49f1a4a21f6c8b4567'
     mock_client.statistics.find_one.side_effect = RepoNotFoundError()
 
     repository = MongoStatisticRepo(mock_client)
     try:
+        # Act
         result = await repository.delete_session(record_id)
     except Exception as e:
+        # Assert
         assert isinstance(e, RepoNotFoundError)
 
 
